72975ff2231cdf21710f7e99489fb17cd2972315
leesouthwart <leesouthwart@gmail.com>
2024-03-07 21:03:51 +0000

    Pagination added

--- app/Http/Livewire/ListItemList.php
+++ app/Http/Livewire/ListItemList.php
@@ -5,22 +5,26 @@ namespace App\Http\Livewire;
 use Illuminate\Database\Eloquent\Collection;
 use Livewire\Component;
 
+use Livewire\WithPagination;
+
 use App\Models\ListItemStatus;
 use App\Models\ListItem;
 
 class ListItemList extends Component
 {
-    public Collection $listItems;
+    use WithPagination;
 
-    public $listeners = ['taskDeleted' => 'mount', 'listItemAdded' => 'mount'];
+    public $listeners = ['taskDeleted' => 'render', 'listItemAdded' => 'render'];
 
-    public function mount()
-    {
-        $this->listItems = ListItem::where('list_item_status_id', '!=', ListItemStatus::DELETED)->get();
-    }
+//    public function mount()
+//    {
+//        $this->listItems = ListItem::where('list_item_status_id', '!=', ListItemStatus::DELETED)->paginate(10);
+//    }
 
     public function render()
     {
-        return view('livewire.list-item-list');
+        return view('livewire.list-item-list', [
+            'listItems' => ListItem::where('list_item_status_id', '!=', ListItemStatus::DELETED)->paginate(10)
+        ]);
     }
 }
--- resources/views/livewire/list-item-list.blade.php
+++ resources/views/livewire/list-item-list.blade.php
@@ -12,7 +12,7 @@
     @foreach($listItems as $listItem)
         <div class="row border-top border-gray align-items-center py-3 @if($listItem->list_item_status_id != \App\Models\ListItemStatus::PENDING) finished-task @endif">
             <div class="col-1">
-                <p class="mb-0">{{$loop->iteration}}</p>
+                <p class="mb-0">{{$listItems->currentPage() == 1 ? $loop->iteration : $loop->iteration + (($listItems->currentPage() - 1) * 10)}}</p>
             </div>
 
             <div class="col-11">
@@ -26,4 +26,6 @@
             <p class="text-center">No tasks found</p>
         </div>
     @endif
+
+    {{ $listItems->links() }}
 </div>
